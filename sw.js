71054a839f5e81c640666f92e72cddeaa701550b
Lucía Giglio <42013988+luciagiglio@users.noreply.github.com>
2021-05-20 15:45:22 -0300

    -

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-self.addEventListener("install", e=>{
+self.addEventListener('install', e=>{
     e.waitUntil(
-        caches.open("static").then(cache=>{
+        caches.open('static').then(cache=>{
             return cache.addAll(['./', './js/modal.js', './js/navbar.js','./src/index.js','./css/style.css', 'favicon.ico', './css/bootstrap.min.css'
             ,'./img/delightin-dee-_WI3DGseB3g-unsplash.jpg','./img/delightin-dee-HF_Q6QcTolw-unsplash.jpg', './img/delightin-dee-jHjMWJq2XqU-unsplash.jpg',
         './img/delightin-dee-JSE2mRfuan8-unsplash.jpg','./img/louis-hansel-0sYLBZjgTTw-unsplash.jpg']);
@@ -11,7 +11,7 @@ self.addEventListener("install", e=>{
 });
 
 
-self.addEventListener("fetch", e=>{
+self.addEventListener('fetch', e=>{
     e.respondWith(
         caches.match(e.request).then(response=>{
             return response || fetch(e.request);
